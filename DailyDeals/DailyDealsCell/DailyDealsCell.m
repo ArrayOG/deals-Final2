1d2443d98e3d28a90f5b6751d6c27470957ef852
malna1r <mal.nnair@gmail.com>
2015-08-11 10:56:01 -0700

    FIXED THE CELL

--- DailyDeals/DailyDealsCell/DailyDealsCell.m
+++ DailyDeals/DailyDealsCell/DailyDealsCell.m
@@ -10,6 +10,8 @@
 
 @implementation DailyDealsCell
 
+@synthesize imageView;
+
 - (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
 {
     self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
